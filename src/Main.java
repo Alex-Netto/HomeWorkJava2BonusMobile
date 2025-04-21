2d638f36b1c36cb85d75f03ff8476a50ffb70832
Alex.Net <alcatras118@gmail.com>
2025-04-21 17:06:14 +0300

    rechange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int deposit = 100; // начальный депозит
-        int donate = 28000; // сумма пополнения счета
+        int donate = 1000; // сумма пополнения счета
         int bonus = donate  / 100; // количество бонусов
-        if (donate > 999) {
+        if (donate > 1000) {
             System.out.println("Сумма на вашем счете " + (deposit + donate + bonus) + "руб");
             System.out.println(" Количество начисленных оператором бонусов " + bonus +"руб");
         } else {
             System.out.println(" Бонусы не начислены ");
+            System.out.println(" Сумма на вашем счете " + (deposit + donate) + "руб");
+
         }
     }
 }
